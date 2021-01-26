14c99f7e0424d12cfe5e67b5dfdf57e3f74a9a3d
Christopher Fraser <cfraz89@gmail.com>
2021-01-26 15:15:48 +0800

    Set up sanity schema

---
+++ template/studio/schemas/landingPage.js
@@ -0,0 +1,12 @@
+export default {
+  name: "landingPage",
+  title: "Landing Page",
+  type: "document",
+  fields: [
+    {
+      name: "seo",
+      title: "SEO",
+      type: "seo",
+    },
+  ],
+};
---
+++ template/studio/schemas/site.js
@@ -0,0 +1,12 @@
+export default {
+  name: 'site',
+  title: 'Site',
+  type: 'document',
+  fields: [
+    {
+      name: 'logo',
+      title: 'logo',
+      type: 'image',
+    },
+  ],
+}
