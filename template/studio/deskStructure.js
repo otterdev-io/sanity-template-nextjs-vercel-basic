cf6657b7b1862b24d549af946d0c6b9892533d77
Christopher Fraser <cfraz89@gmail.com>
2021-02-02 18:49:09 +0800

    Fix preview

--- template/studio/deskStructure.js
+++ template/studio/deskStructure.js
@@ -4,19 +4,15 @@ import * as React from "react";
 import { FaSitemap, FaHome } from "react-icons/fa";
 
 function SitePreview({ document, options }) {
-  const local = process.env.NODE_ENV == "development";
-  if (!process.env.SANITY_STUDIO_SITE_LOCAL_URL) {
+  if (!process.env.SANITY_STUDIO_SITE_URL) {
     console.warn(
-      "SANITY_STUDIO_SITE_LOCAL_URL should be set for preview to work! Falling back to localhost:3000"
+      "SANITY_STUDIO_SITE_URL should be set for preview to work! Falling back to localhost:3000"
     );
   }
   return (
     <iframe
       src={
-        (local
-          ? (process.env.SANITY_STUDIO_SITE_LOCAL_URL ?? "http://localhost:3000")
-          : process.env.VERCEL_URL) +
-        `/api/preview?secret=${process.env.SANITY_STUDIO_PREVIEW_TOKEN}&slug=${options.slug}`
+          `${process.env.SANITY_STUDIO_SITE_URL ?? "http://localhost:3000"}/api/preview?secret=${process.env.SANITY_STUDIO_PREVIEW_TOKEN}&slug=${options.slug}`
       }
       style={{ width: "100%", height: "100%", border: 0 }}
     />
