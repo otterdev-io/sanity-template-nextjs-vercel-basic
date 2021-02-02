d29be88f64ff2ecb45b719427a54e1bb104ff1a3
Christopher Fraser <cfraz89@gmail.com>
2021-02-02 18:53:18 +0800

    preview work

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ set the following vars on vercel:
 NEXT_PUBLIC_SANITY_DATASET
 NEXT_PUBLIC_SANITY_PROJECT_ID
 SANITY_STUDIO_PREVIEW_TOKEN
+SANITY_STUDIO_PREVIEW_URL
 SANITY_API_TOKEN - create at manage.sanity.io
 
 create .env.local:
@@ -18,7 +19,7 @@ SANITY_API_TOKEN
 create studio/.env.development and .env.production:
 SANITY_STUDIO_API_PROJECT_ID
 SANITY_STUDIO_API_DATASET
-SANITY_STUDIO_SITE_LOCAL_URL="http://localhost:3000"
+SANITY_STUDIO_PREVIEW_URL="http://localhost:3000"
 SANITY_STUDIO_PREVIEW_TOKEN
 
 add "http://localhost:3000" as CORS url at manage.sanity.io
--- template/studio/deskStructure.js
+++ template/studio/deskStructure.js
@@ -4,15 +4,15 @@ import * as React from "react";
 import { FaSitemap, FaHome } from "react-icons/fa";
 
 function SitePreview({ document, options }) {
-  if (!process.env.SANITY_STUDIO_SITE_URL) {
+  if (!process.env.SANITY_STUDIO_PREVIEW_URL) {
     console.warn(
-      "SANITY_STUDIO_SITE_URL should be set for preview to work! Falling back to localhost:3000"
+      "SANITY_STUDIO_PREVIEW_URL should be set for preview to work! Falling back to localhost:3000"
     );
   }
   return (
     <iframe
       src={
-          `${process.env.SANITY_STUDIO_SITE_URL ?? "http://localhost:3000"}/api/preview?secret=${process.env.SANITY_STUDIO_PREVIEW_TOKEN}&slug=${options.slug}`
+          `${process.env.SANITY_STUDIO_PREVIEW_URL ?? "http://localhost:3000"}/api/preview?secret=${process.env.SANITY_STUDIO_PREVIEW_TOKEN}&slug=${options.slug}`
       }
       style={{ width: "100%", height: "100%", border: 0 }}
     />
